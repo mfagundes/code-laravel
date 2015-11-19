629f6ff2aad7774c8c014dc918751e05d15ccd91
mauricio <mauricio.fagundes@gmail.com>
2015-11-19 14:06:36 -0300

    uploading file

---
+++ app/Http/Controllers/ProjectFileController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use Illuminate\Http\Request;
+use CodeProject\Http\Requests;
+use CodeProject\Http\Controllers\Controller;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+class ProjectFileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $file = $request->file('file');
+        $extension = $file->getClientOriginalExtension();
+
+        Storage::put($request->name . "." . $extension, File::get($file));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,13 +23,15 @@ Route::group(['middleware'=>'oauth'], function() {
     Route::resource('client', 'ClientController', ['except'=>['create', 'edit]']]);
     Route::resource('project', 'ProjectController', ['except'=>['create', 'edit']]);
 
-    Route::group(['prefix'=>'project'], function() {
+     Route::group(['prefix'=>'project'], function() {
 
         Route::get('{id}/note', 'ProjectNoteController@index');
         Route::post('{id}/note', 'ProjectNoteController@store');
         Route::get('{id}/note/{noteId}', 'ProjectNoteController@show');
         Route::put('{id}/note/{noteId}', 'ProjectNoteController@update');
         Route::delete('{id}/note/{noteId}', 'ProjectNoteController@@destroy');
+
+        Route::post('{id}/file', 'ProjectFileController@store');
 
         Route::get('{id}/task', 'ProjectTaskController@index');
         Route::post('{id}/task', 'ProjectTaskController@store');
